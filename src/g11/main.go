83452dbd6205b0deb66116fec54ba184348a71e8
shidu <10983399+shiwudu@user.noreply.gitee.com>
2023-05-21 15:04:46 +0800

    添加练习代码注释

--- src/g11/main.go
+++ src/g11/main.go
@@ -6,12 +6,15 @@ import (
 	"strings"
 )
 
+// 添加 参数，设置参数名称，参数默认值，参数的作用
 var n = flag.Bool("n", false, "不知到啊")
 var sep = flag.String("s", " ", "你知道吗")
 
 func main() {
+	//flag.Parse函数，用于更新每个标志参数对应变量的值（之前是默认值）
 	flag.Parse()
 	//flag.Args() 返回命令行参数中不包含标志的部分，并以字符串切片的形式返回。
+	//程序中的 sep 和 n 变量分别是指向对应命令行标志参数变量的指针，因此必须用 *sep 和 *n 形式的指针语法间接引用它们
 	fmt.Print(strings.Join(flag.Args(), *sep))
 	if !*n {
 		fmt.Println()
--- src/g12/main.go
+++ src/g12/main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	//new创建初始变量地址
 	p := new(int)
 	*p = 1
+	fmt.Println(p)
 	fmt.Println(*p)
 }
